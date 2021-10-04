9c3b1f506a3bdad4f81ecf3c0d9fff4f80fda1e5
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2021-10-04 18:03:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,20 +16,20 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+  await event.reply("I'm MentionAll Bot, I can mention almost all members in group or channel \nClick **/help** for more information",
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('Updates', 'https://t.me/Hazard_Bots'),
+                      Button.url('Support', 'https://t.me/HazardBot_support')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\nYou can use this command with text what you want to mention others.\n`Example: /mentionall Good Morning!`\nYou can you this command as a reply to any message. Bot will tag users to that replied messsage.\n"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('Updates', 'https://t.me/Hazard_Bots'),
+                      Button.url('Support', 'https://t.me/HazardBot_support')]
                     ),
                     link_preview=False
                    )
@@ -37,13 +37,13 @@ async def help(event):
 @client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
 async def mentionall(event):
   if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
+    return await event.respond("This command can be use in groups and channels!")
   
   admins = []
   async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
     admins.append(admin.id)
   if not event.sender_id in admins:
-    return await event.respond("__Only admins can mention all!__")
+    return await event.respond("Only admins can mention all!")
   
   if event.pattern_match.group(1):
     mode = "text_on_cmd"
@@ -52,11 +52,11 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = event.reply_to_msg_id
     if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+        return await event.respond("I can't mention members for older messages! (messages which are sent before I'm added to group)")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__Give me one argument!__")
+    return await event.respond("Give me one argument!")
   else:
-    return await event.respond("__Reply to a message or give me some text to mention others!__")
+    return await event.respond("Reply to a message or give me some text to mention others!")
   
   if mode == "text_on_cmd":
     usrnum = 0
